ef20b1b18c4539f1e0d8f1538f7b534bc3282888
Joshua Zito <jz4396@gmail.com>
2019-01-09 09:50:56 -0500

    Added easy function to list categories from label map

--- CROWSSS/PythonApplication1/Detection.py
+++ CROWSSS/PythonApplication1/Detection.py
@@ -5,7 +5,6 @@ test = Objdet("..\..\object_detection")
 
 test.downloadGraph("ssd_mobilenet_v1_coco_11_06_2017","..\..\object_detection\data\mscoco_label_map.pbtxt",90)
 
-if(test.live_Detection(0)==-1):
-    cv2.imshow("failure",test.image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+test.listCategories()
+
+test.live_Detection(0)
